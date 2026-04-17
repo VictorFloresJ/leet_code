d4275060807b62b18f218c0220ebd35426388f50
Víctor <victor_2002fj@hotmail.com>
2026-04-17 12:39:53 -0600

    191

---
+++ 191_number_of_1_bits/Solution.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int hammingWeight(int n) {
+        int cociente = n;
+        ArrayList<Integer> residuos = new ArrayList<>();
+        while(cociente!= 0) {
+            int residuo = cociente % 2;
+            residuos.add(residuo);
+            cociente = cociente / 2;
+        }
+        int numsOfOnes = 0;
+        for (Integer residuo : residuos) {
+            if (residuo == 1) numsOfOnes++;
+        }
+        return numsOfOnes;
+    }
+
+    public int hammingWeightOpt(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = n & (n-1);
+        }
+        return count;
+    }
+
+/*
+    public int hammingWeight(int n) {
+
+        int count = 0;
+
+        while(n != 0){
+            count++;
+
+            n &= (n - 1);
+        }
+
+        return count;
+        
+    }
+*/
+
+    public void main(String[] args) {
+        System.out.println(hammingWeight(2147483645));
+        System.out.println(hammingWeightOpt(2147483645));
+    }
+}
